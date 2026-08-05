1a79a463b710781d68ac4d08966966ceedd46cda
shaikhaftab0444-dev <shaikhaftab0444@gmail.com>
2026-08-05 15:07:18 +0530

    ads

--- obj/Release/net8.0/ERP-System.AssemblyInfo.cs
+++ obj/Release/net8.0/ERP-System.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP-System")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38500500a8745ac5002df77217d9786107b1c6e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP-System")]
