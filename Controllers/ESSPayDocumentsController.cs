f58a2cdf3c0344fcbba52817413c949c810ecb72
hamidmajid917-afk <hamidmajid917@gmail.com>
2026-09-14 12:02:16 +0530

    Dev 2: 14-9-2026

--- Controllers/ESSPayDocumentsController.cs
+++ Controllers/ESSPayDocumentsController.cs
@@ -29,6 +29,8 @@ namespace ERP_System.Controllers
                 return id;
             }
             return 1;
+        }
+
         // GET: /ESSPayDocuments or /ESSPayDocuments/Index
         [HttpGet]
         public IActionResult Index()
